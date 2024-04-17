44608cc42ea8b16e9d231bbf553868a22e57b090
Manthan Mallikarjun <nahtnam@gmail.com>
2024-04-17 00:57:12 -0700

    add comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 export default function Home() {
+  // To reproduce, run the server, then add `prose-sm`, `prose-lg`, or `prose-xl`
+  // You will notice that it does not generate the classes for it
   return (
-    <main className="prose prose-xl">
+    <main className="prose">
       <h1>hello world</h1>
     </main>
   );
